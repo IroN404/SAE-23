3df72f1cbff25bfe831ca5167d31b8d5d0cbc188
gregory <g.maitre2810@icloud.com>
2022-06-14 04:00:30 +0200

    ajout produit dans categorie

--- modele/drive/forms.py
+++ modele/drive/forms.py
@@ -18,12 +18,11 @@ class categorieform(ModelForm):
 class produitform(ModelForm):
     class Meta:
         model = models.produit
-        fields = ('nom','date_peremption', 'photo','marque','auteur','categorie')
+        fields = ('nom','date_peremption', 'photo','marque','auteur')
         labels = {
             'nom': _('Nom'),
             'date_peremption' : _('Date de péremption'),
             'photo' : _('Photo') ,
             'marque' : _('Marque'),
             'auteur': _('Auteur'),
-            'categorie': _('categorie'),
         }
